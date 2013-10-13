190904c65dbbce72a324b8063b81b4ca9a31b1cf
stenders <stenders@163.com>
2013-10-13 13:25:18 +0800

    events update

--- events.js
+++ events.js
@@ -1,50 +1,49 @@
-var Events = function(){
-	var ev = function(){
-		this.list = {};
-	};
-	ev.prototype = {
-		constructor: ev,
-		on : function(type, func) {
-			this.list[type] = this.list[type] || [];
-			this.list[type].push(func);
-			return this;
-		},
-		trigger : function(type){
-			var list = this.list[type] || [],
-				args = list.slice.call(arguments, 1);
-			if(list.length){
-				for(var i = 0, len = list.length; i < len; i ++){
-					list[i].apply(null, args);
-				}
-			}
-			return this;
-		},
-		off : function(type, func){
-			if(type){
-				var list = this.list[type];
-				if(!func){
-					delete this.list[type];
-				} else if(list) {
-					for(var i = 0, len = list.length; i < len; i ++){
-						if(func === list[i]) {
-							list.splice(i, 1);
-							break;
-						}
-					}
-				}
-			} else {
-				this.list = {};
-			}
-			return this;
-		},
-		once : function(type, func){
-			var that = this,
-				one = function(){
-					that.off(type);
-					func.apply(null, arguments);
-				};
-			return this.on(type, one);
-		}
-	};
-	return ev;
-}();
+var Events = function() {
+  function ev(){
+    this.list = {}
+  }
+  ev.prototype = {
+    on : function(type, handler) {
+      if(typeof handler != 'function'){
+        throw new Error('invalid handler.')
+      }
+      this.list[type] = this.list[type] || []
+      this.list[type].push(handler)
+      return this
+    },
+    off : function(type, handler) {
+      var len = arguments.length
+      var list = this.list[type]
+      if(len == 0){
+        this.list = {}
+      } else if(len == 1) {
+        delete this.list[type]
+      } else if(list) {
+        for(len = 0; len < list.length; len ++){
+          if(list[len] === handler) list.splice(len, 1)
+        }
+      }
+      return this
+    },
+    trigger : function (type){
+      var list = this.list[type] || []
+      var args = list.slice.call(arguments, 1)
+      for(var i = 0, len = list.length; i < len; i ++){
+        list[i].apply(null, args)
+      }
+      return this
+    },
+    once : function (type, handler) {
+      if(typeof handler != 'function'){
+        throw new Error('invalid handler.')
+      }
+      var that = this
+      var one = function(){
+        handler.apply(null, arguments)
+        that.off(type, one)
+      }
+      return this.on(type, one)
+    }
+  }
+  return ev
+}()
