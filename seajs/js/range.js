91ecc0647e0186f642553e057710ea619658ef0a
stenders <stenders@163.com>
2013-11-26 19:00:40 +0800

    seajs learning.

---
+++ seajs/js/range.js
@@ -0,0 +1,17 @@
+// D
+
+define(function(require, exports, module){
+
+  function range(iNum, iMax, iMin){
+    if(iNum > iMax){
+      return iMax
+    } else if( iNum < iMin){
+      return iMin
+    } else {
+      return iNum
+    }
+  }
+
+  exports.range = range
+
+})
